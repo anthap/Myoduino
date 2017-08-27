ee1664f2e23a7d3194e53ecb143a86f1ff44f235
anthap <anth.pasq@gmail.com>
2017-08-27 13:20:01 -0400

    Initial Upload

---
+++ MEbot/Myo/Move to Myoduino/MyoController.h
@@ -0,0 +1,37 @@
+
+#ifndef MyoController_H
+#define MyoController_H
+
+#include "Arduino.h"
+
+#define DEBUG	0
+
+enum Poses{
+	rest,
+	fist,
+	waveIn,
+	waveOut,
+	fingersSpread,
+	doubleTap,
+	unknown
+};
+
+class MyoController {
+public:
+
+    /* Initialization methods */
+    MyoController();
+    ~MyoController();
+    bool initMyo();
+    bool updatePose();
+    Poses getCurrentPose();
+	int getCurrentPitch();
+private:
+	Poses current_pose_;
+	String storageStr;
+	String storagePitchVal;
+	String msgChar;
+	int current_pitch_;
+};
+
+#endif
---
+++ MEbot/Sweep/Sweep.ino
@@ -0,0 +1,41 @@
+#include <Servo.h> 
+
+int servoPin = 6;
+ 
+Servo servo;  
+ 
+int angle = servo.read();   // servo position in degrees 
+ 
+void setup() 
+{ 
+  servo.attach(servoPin); 
+  Serial.begin(9600); 
+} 
+ 
+ 
+void loop() 
+{ 
+  //start at the correct location
+  if(angle > 0)
+  {
+    for(angle; angle > 0; angle--)    
+    {                                
+      servo.write(angle);           
+      delay(20);   
+    } 
+  }
+  
+  // scan from 0 to 180 degrees
+  for(angle = 0; angle < 180; angle++)  
+  {                                  
+    servo.write(angle);     
+    delay(20);    
+  } 
+  
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 0; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(20);   
+  } 
+} 
